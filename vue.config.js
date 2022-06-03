1fe33bf3117755b3df22ab56acaa6812966fc7d7
DidiRoyadi123 <didiroyadi123@gmail.com>
2022-06-03 19:19:37 +0700

    delete multi word on vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  'vue/multi-word-component-names': ['error', {
-    'ignores': ['default']
-  }]
 })
