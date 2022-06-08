9681ae04c175d8ec0e56ff9edd7f19590c4ff546
DidiRoyadi123 <didiroyadi123@gmail.com>
2022-06-08 18:36:52 +0700

    push sementara beranda

--- src/views/berandaView.vue
+++ src/views/berandaView.vue
@@ -5,7 +5,18 @@
     <br>
     <br>
     <br>
-    <h1>This is an Beranda page</h1>
+    <breadcrumbComponentVue/>
+    <b-card-group class="box">
+     <b-card bg-variant="success" text-variant="white" class="text-center" >
+        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+      </b-card>
+      <b-card bg-variant="success" text-variant="white" class="text-center">
+        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+      </b-card>
+      <b-card bg-variant="success" text-variant="white" class="text-center">
+        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+      </b-card>
+    </b-card-group>
     <Footer />
   </div>
 </template>
@@ -15,14 +26,30 @@
 import Navbar from '@/components/navbarComponent.vue'
 import Sidebar from '@/components/sidebarComponent.vue'
 import Footer from '@/components/footerComponent.vue'
+import breadcrumbComponentVue from '../components/breadcrumbBeranda.vue'
+
 
 export default {
   name: 'berandaView',
   components: {
     Navbar,
+    breadcrumbComponentVue,
     Sidebar,
     Footer
   },
 
 }
 </script>
+
+<style scoped>
+body {
+  background-color:#E5E5E5 !important;
+}
+.text-center {
+  border:20px solid #E5E5E5 !important;
+}
+.box {
+  height: 200px;
+  background: #1CAB59 !important;
+}
+</style>
