e132f4a9f6209c685cbf6768e0a5fc76401f4d34
Savira Auliya Ardana <savirauliya10@gmail.com>
2022-06-08 19:45:40 +0700

    feat: slicing

---
+++ src/views/addGedung.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="list">
+    <Navbar />
+    <Sidebar />
+    <br>
+    <br>
+    <br>
+    <h1>Halaman Tambah gedung</h1>
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/navbarComponent.vue'
+import Sidebar from '@/components/sidebarComponent.vue'
+import Footer from '@/components/footerComponent.vue'
+
+export default {
+  name: 'addGedung',
+  components: {
+    Navbar,
+    Sidebar,
+    Footer
+  },
+
+}
+</script>
---
+++ src/views/kelolaGedung.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="list">
+    <Navbar />
+    <Sidebar />
+    <br>
+    <br>
+    <br>
+    <h1>Halaman Kelola Gedung</h1>
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/navbarComponent.vue'
+import Sidebar from '@/components/sidebarComponent.vue'
+import Footer from '@/components/footerComponent.vue'
+
+export default {
+  name: 'kelolaGedung',
+  components: {
+    Navbar,
+    Sidebar,
+    Footer
+  },
+
+}
+</script>
