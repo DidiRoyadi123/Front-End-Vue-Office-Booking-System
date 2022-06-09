1e82a60a0396bfcc42ce2e545761188346aba89e
DidiRoyadi123 <didiroyadi123@gmail.com>
2022-06-09 15:11:33 +0700

    add icon di card beranda

---
+++ src/components/breadcrumbBeranda.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="Beranda">
+    <b-card>
+      <div>
+        <b-breadcrumb>
+          <b-breadcrumb-item to="/beranda" active> Beranda</b-breadcrumb-item>
+        </b-breadcrumb>
+      </div>
+    </b-card>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'breadCrumbComponent',
+}
+</script>
+
+<style>
+body {
+  background-color: #E5E5E5 !important;
+}
+.card-body {
+  padding: 17px 0px 0px 20px !important;
+}
+.breadcrumb>li+li:before {
+  color: #ccc !important;
+  content: "> " !important;
+}
+</style>
