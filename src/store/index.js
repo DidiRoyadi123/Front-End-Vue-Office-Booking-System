f72dfb1263980bd6474ba5c76d7af4dc3bcf9dfc
DidiRoyadi123 <didiroyadi123@gmail.com>
2022-06-27 14:33:39 +0700

    add axios on store

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,36 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+Vue.use(VueAxios, axios);
 
 export default new Vuex.Store({
   state: {
+    items: [],
   },
   getters: {
+    items: (state) => {
+      return state.items
+    },
   },
   mutations: {
+    setItems: (state, items) => {
+      state.items = items;
+    }
   },
   actions: {
+    loadItems ({ commit }) {
+      axios
+          .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+          .then(response => response.data)
+          .then(items => {
+              console.log(items);
+          commit('SET_Items', items)
+      })
+  }
   },
   modules: {
-  }
-})
+  },
+});
