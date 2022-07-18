da8559fad5d7b3fd862d683a4b59ca2c0d95ec70
DedeMaulana9 <99723127+DedeMaulana9@users.noreply.github.com>
2022-07-18 21:25:38 +0700

    integrasi kelolaPesanan

--- src/views/kelolaPesanan.vue
+++ src/views/kelolaPesanan.vue
@@ -2,36 +2,64 @@
   <div class="list">
     <Navbar />
     <Sidebar />
-    <br>
-    <br>
-    <br>
+    <br />
+    <br />
+    <br />
     <Breadcrumb />
     <b-card>
       <p>Kelola Pesanan</p>
-      <b-table 
-        striped hover 
-        :items="items" 
-        :fields="fields"
-      >
-      <template #cell(Aksi)>
-         <b-button variant="success" @click="editGedung(gedung.id_gedung)">Terima</b-button>
-          <b-button variant="danger" @click="deleteGedung(gedung.id_gedung)">Batalkan</b-button>
-      </template>
-      </b-table>
-
+      <b-table-simple striped hover>
+        <b-thead>
+          <b-tr>
+            <b-th>No</b-th>
+            <b-th>Kode Pemesanan</b-th>
+            <b-th>Nama Gedung</b-th>
+            <b-th>No Handphone</b-th>
+            <b-th>Jumlah Pesanan</b-th>
+            <b-th>Total Harga</b-th>
+            <b-th>Tanggal Masuk</b-th>
+            <b-th>Tanggal Keluar</b-th>
+            <b-th>Order Status</b-th>
+          </b-tr>
+        </b-thead>
+        <b-tbody>
+          <b-tr v-for="(booking, id) in bookings" :key="id">
+            <b-td>{{ booking.id }}</b-td>
+            <b-td>{{ booking.bookingcode }}</b-td>
+            <b-td>{{ booking.namagedung }}</b-td>
+            <b-td>{{ booking.nama }}</b-td>
+            <b-td>{{ booking.phone }}</b-td>
+            <b-td>{{ booking.jumlah_pesanan }}</b-td>
+            <b-td>{{ booking.total_harga }}</b-td>
+            <b-td>{{ booking.checkin }}</b-td>
+            <b-td>{{ booking.checkout }}</b-td>
+            <b-td>{{ booking.status }}</b-td>
+            <b-td>
+              <b-button variant="success" @click="okGedung(pesanan.id_gedung)">Terima</b-button>
+              <b-button v-b-modal.hapus variant="danger">Batalkan</b-button>
+              <b-modal id="hapus" centered busy>
+                <p class="my-4">Apakah Anda Yakin ?</p>
+                <b-button variant="danger" @click="cancelGedung(gedung.id)">Ya</b-button>
+                <b-button variant="success" @click="$bModal.hide('hapus')">Tidak</b-button>
+              </b-modal>
+            </b-td>
+          </b-tr>
+        </b-tbody>
+      </b-table-simple>
     </b-card>
-    <liveChatBtn/>
+    <liveChatBtn />
     <FooterComponent />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Navbar from '@/components/navbarComponent.vue'
-import Sidebar from '@/components/sidebarComponent.vue'
-import Breadcrumb from '../components/breadcrumb.vue'
-import FooterComponent from '@/components/footerComponent.vue'
-import LiveChatBtn from '@/components/liveChatBtn.vue'
+import Navbar from '@/components/navbarComponent.vue';
+import Sidebar from '@/components/sidebarComponent.vue';
+import Breadcrumb from '../components/breadcrumb.vue';
+import FooterComponent from '@/components/footerComponent.vue';
+import LiveChatBtn from '@/components/liveChatBtn.vue';
+import axios from 'axios';
 
 export default {
   name: 'daftarPengguna',
@@ -40,68 +68,60 @@ export default {
     Sidebar,
     Breadcrumb,
     FooterComponent,
-    LiveChatBtn
+    LiveChatBtn,
   },
-    data() {
+  data() {
     return {
-      fields: ['ID_Pemesanan','Nama', 'NomorHp','ID_Email' ,'Nama_Gedung','Jumlah_Pemesanan','Total_Harga','Status', 'Aksi'],
-      items: [
-        { ID_Pemesanan: 1, Nama: 'Rara', NomorHp: '085000000000',ID_Email: 'andi123@gmail.com' ,Nama_Gedung:'Hotel',Jumlah_Pemesanan:'3 hari',Total_Harga: 'Rp.2.000.000' ,Status:'Belum Ada'},
-        { ID_Pemesanan: 2, Nama: 'Riri ', NomorHp: '085000000000',ID_Email: 'andi123@gmail.com' ,Nama_Gedung:'Hotel',Jumlah_Pemesanan:'3 hari',  Total_Harga: 'Rp.2.000.000',Status:'Belum Ada'},
-        { ID_Pemesanan: 3, Nama: 'Bayu', NomorHp: '085000000000',ID_Email: 'andi123@gmail.com' ,Nama_Gedung:'Hotel',Jumlah_Pemesanan:'3 hari', Total_Harga: 'Rp.2.000.000',Status:'Belum Ada' },
-        { ID_Pemesanan: 4, Nama: 'Adit', NomorHp: '085000000000',ID_Email: 'andi123@gmail.com' ,Nama_Gedung:'Hotel',Jumlah_Pemesanan:'3 hari', Total_Harga: 'Rp.2.000.000' ,Status:'Belum Ada'},
-        { ID_Pemesanan: 5, Nama: 'Bagus', NomorHp: '085000000000',ID_Email: 'andi123@gmail.com' ,Nama_Gedung:'Hotel',Jumlah_Pemesanan:'3 hari', Total_Harga: 'Rp.2.000.000' ,Status:'Belum Ada'},
-        { ID_Pemesanan: 6, Nama: 'Sinta', NomorHp: '085000000000',ID_Email: 'andi123@gmail.com' ,Nama_Gedung:'Hotel',Jumlah_Pemesanan:'3 hari', Total_Harga: 'Rp.2.000.000',Status:'Belum Ada' },
-        { ID_Pemesanan: 7, Nama: 'Xira', NomorHp: '085000000000',ID_Email: 'andi123@gmail.com' ,Nama_Gedung:'Hotel',Jumlah_Pemesanan:'3 hari', Total_Harga: 'Rp.2.000.000' ,Status:'Belum Ada'},
-        { ID_Pemesanan: 8, Nama: 'Yanti', NomorHp: '085000000000',ID_Email: 'andi123@gmail.com' ,Nama_Gedung:'Hotel',Jumlah_Pemesanan:'3 hari', Total_Harga: 'Rp.2.000.000' ,Status:'Belum Ada'},
-        { ID_Pemesanan: 9, Nama: 'Putra', NomorHp: '085000000000',ID_Email: 'andi123@gmail.com' ,Nama_Gedung:'Hotel',Jumlah_Pemesanan:'3 hari', Total_Harga: 'Rp.2.000.000' ,Status:'Belum Ada'},
-        { ID_Pemesanan: 10, Nama: 'Kamal', NomorHp: '085000000000',ID_Email: 'andi123@gmail.com' ,Nama_Gedung:'Hotel',Jumlah_Pemesanan:'3 hari', Total_Harga: 'Rp.2.000.000',Status:'Belum Ada' },
-        { ID_Pemesanan: 11, Nama: 'Reza', NomorHp: '085000000000',ID_Email: 'andi123@gmail.com' ,Nama_Gedung:'Hotel',Jumlah_Pemesanan:'3 hari', Total_Harga: 'Rp.2.000.000',Status:'Belum Ada' },
-        { ID_Pemesanan: 12, Nama: 'Dino', NomorHp: '085000000000',ID_Email: 'andi123@gmail.com' ,Nama_Gedung:'Hotel',Jumlah_Pemesanan:'3 hari', Total_Harga: 'Rp.2.000.000',Status:'Belum Ada' },
-        { ID_Pemesanan: 13, Nama: 'Dino', NomorHp: '085000000000',ID_Email: 'andi123@gmail.com' ,Nama_Gedung:'Hotel',Jumlah_Pemesanan:'3 hari', Total_Harga: 'Rp.2.000.000' ,Status:'Belum Ada'},
-        { ID_Pemesanan: 14, Nama: 'Cia', NomorHp: '085000000000',ID_Email: 'andi123@gmail.com' ,Nama_Gedung:'Hotel',Jumlah_Pemesanan:'3 hari', Total_Harga: 'Rp.2.000.000' ,Status:'Belum Ada'},
-        { ID_Pemesanan: 15, Nama: 'Carney', NomorHp: '085000000000',ID_Email: 'andi123@gmail.com' ,Nama_Gedung:'Hotel',Jumlah_Pemesanan:'3 hari', Total_Harga: 'Rp.2.000.000',Status:'Belum Ada' },
+      bookings: [],
+    };
+  },
 
-      ]
-    }
-  }
-
-}
+  mounted() {
+    axios
+      .get('https://officebooking-app-pn6n3.ondigitalocean.app/admin/bookings')
+      .then((response) => {
+        this.gedung = response.data.data;
+        console.log(this.gedung);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
 </script>
 
 <style scoped>
-.list{
+.list {
   background-color: #e5e5e5;
 }
-.card{
- margin-right: 25px;
- margin-left: 25px;
- margin-bottom: 25px;
+.card {
+  margin-right: 25px;
+  margin-left: 25px;
+  margin-bottom: 25px;
 }
-p{
+p {
   font-family: 'Poppins';
   font-style: normal;
   font-size: 19px;
-  font-weight:600;
+  font-weight: 600;
   line-height: 33px;
   display: flex;
-  color: #1CAB59;
+  color: #1cab59;
 }
 .btn-success {
-  background-color: #1CAB59;
-  border-color: #1CAB59;
+  background-color: #1cab59;
+  border-color: #1cab59;
   width: 90px;
   height: 31px;
   margin: 10px !important;
   padding: 0 !important;
 }
 .btn-danger {
-  background-color: #FF3538 !important;
-  border-color: #FF3538 !important;
+  background-color: #ff3538 !important;
+  border-color: #ff3538 !important;
   width: 90px;
   height: 31px;
   margin: 10px !important;
   padding: 0 !important;
 }
-
 </style>
