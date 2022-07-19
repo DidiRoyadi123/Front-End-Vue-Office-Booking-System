c070da52d961970bde5a0eebec182d83292c58a0
Savira Auliya Ardana <savirauliya10@gmail.com>
2022-07-19 11:47:11 +0700

    feat : integrasi buat dan kelola pesanan

--- src/views/addPesanan.vue
+++ src/views/addPesanan.vue
@@ -1,190 +1,271 @@
+<!-- @format -->
+
 <template>
-  <div class="Admin">
-    <Navbar />
-    <Sidebar />
-    <br />
-    <br />
-    <br />
-    <Breadcrumb />
-    <b-card>
-      <p>Pembuatan Pesanan Baru</p>
-      <b-container fluid class="row mb-3">
-        <form class="row" @submit.prevent="simpan">
-          <b-col sm="2" class="row mb-3">
-            <label for="input-small" class="text1"><strong>Jenis Gedung:</strong></label>
-          </b-col>
-          <b-col sm="10">
-            <select class="form-select" id="inputGroupSelect03" aria-label="Example select with button addon" placeholder="Masukkan Jenis gedung">
-              <option selected>Masukkan Jenis Gedung</option>
-              <option value="1" v-for="(nearby, id) in nearby" :key="id">{{ nearby.namefacilities }}</option>
-            </select>
-          </b-col>
-          <b-col sm="2" class="row mb-3">
-            <label for="input-small" class="text1"><strong>Nama:</strong></label>
-          </b-col>
-          <b-col sm="10">
-            <b-form-input v-model="nearby.nama" id="input-small" type="text" size="sm" placeholder="Masukkan Nama Lengkap"></b-form-input>
-          </b-col>
-          <b-col sm="2" class="row mb-3">
-            <label for="input-small" class="text1"><strong>No Handphone:</strong></label>
-          </b-col>
-          <b-col sm="10">
-            <b-form-input v-model="nearby.no_hp" id="input-small" type="text" size="sm" placeholder="Masukkan No Handphone"></b-form-input>
-          </b-col>
-          <b-col sm="2" class="row mb-3">
-            <label for="input-small" class="text1"><strong>Jumlah Pemesanan:</strong></label>
-          </b-col>
-          <b-col sm="10">
-            <b-form-input v-model="nearby.jumlah_pesanan" id="input-small" type="text" size="sm" placeholder="Masukkan Jumlah Pemesanan"></b-form-input>
-          </b-col>
-          <b-col sm="2" class="row mb-3">
-            <label for="input-small" class="text1"><strong>Total Harga:</strong></label>
-          </b-col>
-          <b-col sm="10">
-            <b-form-input v-model="nearby.total_harga" id="input-small" type="text" size="sm" placeholder="Masukkan Total Harga"></b-form-input>
-          </b-col>
-          <b-col sm="2" class="row mb-3">
-            <label for="input-small" class="text1"><strong>Tanggal Masuk:</strong></label>
-          </b-col>
-          <b-col sm="10">
-            <b-form-input id="input-small" type="text" size="sm" placeholder="Masukkan Tanggal Masuk"></b-form-input>
-          </b-col>
-          <b-col sm="2" class="row mb-3">
-            <label for="input-small" class="text1"><strong>Tanggal Keluar:</strong></label>
-          </b-col>
-          <b-col sm="10">
-            <b-form-input id="input-small" type="text" size="sm" placeholder="Masukkan Tanggal Keluar"></b-form-input>
-          </b-col>
-          <b-col sm="2" class="row"> </b-col>
-          <b-col sm="2">
-            <button class="btn btn-success float-right mt-3" type="submit">Buat</button>
-            <button class="btn btn-success float-right mt-3">Batal</button>
-          </b-col>
-        </form>
-      </b-container>
-    </b-card>
-    <FooterComponent />
-  </div>
+	<div class="Admin">
+		<Navbar />
+		<Sidebar />
+		<br />
+		<br />
+		<br />
+		<Breadcrumb />
+		<b-card v-if="loading">
+			<b-spinner variant="success" label="Spinning"></b-spinner>
+		</b-card>
+		<b-card v-else>
+			<p>Pembuatan Pesanan Baru</p>
+			<b-container fluid class="row mb-3">
+				<form class="row" @submit.prevent="simpan">
+					<b-col sm="2" class="row mb-3">
+						<label for="input-small" class="text1"
+							><strong>Nama Gedung:</strong></label
+						>
+					</b-col>
+					<b-col sm="10">
+						<b-form-select
+							class="form-select"
+							v-model="selected"
+							:options="options"
+						></b-form-select>
+					</b-col>
+					<b-col sm="2" class="row mb-3">
+						<label for="input-small" class="text1"
+							><strong>Nama:</strong></label
+						>
+					</b-col>
+					<b-col sm="10">
+						<b-form-input
+							id="input-small"
+							type="text"
+							size="sm"
+							v-model="items.Nama"
+							placeholder="Masukkan Nama Lengkap"
+						></b-form-input>
+					</b-col>
+					<b-col sm="2" class="row mb-3">
+						<label for="input-small" class="text1"
+							><strong>No Handphone:</strong></label
+						>
+					</b-col>
+					<b-col sm="10">
+						<b-form-input
+							id="input-small"
+							type="text"
+							size="sm"
+							v-model="items.NomorHp"
+							placeholder="Masukkan No Handphone"
+						></b-form-input>
+					</b-col>
+					<b-col sm="2" class="row mb-3">
+						<label for="input-small" class="text1"
+							><strong>Jumlah Pemesanan:</strong></label
+						>
+					</b-col>
+					<b-col sm="10">
+						<b-form-input
+							id="input-small"
+							type="text"
+							size="sm"
+							v-model="items.Jumlah_Pemesanan"
+							placeholder="Masukkan Jumlah Pemesanan"
+						></b-form-input>
+					</b-col>
+					<b-col sm="2" class="row mb-3">
+						<label for="input-small" class="text1"
+							><strong>Total Harga:</strong></label
+						>
+					</b-col>
+					<b-col sm="10">
+						<b-form-input
+							id="input-small"
+							type="text"
+							size="sm"
+							v-model="items.Total_Harga"
+							placeholder="Masukkan Total Harga"
+						></b-form-input>
+					</b-col>
+					<b-col sm="2" class="row mb-3">
+						<label for="input-small" class="text1"
+							><strong>Tanggal Masuk:</strong></label
+						>
+					</b-col>
+					<b-col sm="10">
+						<b-form-input
+							id="input-small"
+							type="text"
+							size="sm"
+							v-model="items.Tanggal_Masuk"
+							placeholder="Masukkan Tanggal Masuk"
+						></b-form-input>
+					</b-col>
+					<b-col sm="2" class="row mb-3">
+						<label for="input-small" class="text1"
+							><strong>Tanggal Keluar:</strong></label
+						>
+					</b-col>
+					<b-col sm="10">
+						<b-form-input
+							id="input-small"
+							type="text"
+							size="sm"
+							v-model="items.Tanngal_Keluar"
+							placeholder="Masukkan Tanggal Keluar"
+						></b-form-input>
+					</b-col>
+					<b-col sm="2" class="row"> </b-col>
+					<b-col sm="2">
+						<button class="btn btn-success float-right mt-3" type="submit">
+							Buat
+						</button>
+						<button class="btn btn-success float-right mt-3">Batal</button>
+					</b-col>
+				</form>
+			</b-container>
+			{{ items }}
+		</b-card>
+		<FooterComponent />
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Navbar from '@/components/navbarComponent.vue';
-import Sidebar from '@/components/sidebarComponent.vue';
-import FooterComponent from '@/components/footerComponent.vue';
-import Breadcrumb from '../components/breadcrumb.vue';
-import Vue from 'vue';
-import axios from 'axios';
-import VueAxios from 'vue-axios';
-Vue.use(VueAxios, axios);
+	// @ is an alias to /src
+	import Navbar from "@/components/navbarComponent.vue";
+	import Sidebar from "@/components/sidebarComponent.vue";
+	import FooterComponent from "@/components/footerComponent.vue";
+	import Breadcrumb from "../components/breadcrumb.vue";
+	import Vue from "vue";
+	import axios from "axios";
+	import VueAxios from "vue-axios";
+	Vue.use(VueAxios, axios);
 
-export default {
-  name: 'profilAdmin',
-  components: {
-    Navbar,
-    Sidebar,
-    FooterComponent,
-    Breadcrumb,
-  },
-  data() {
-    return {
-      nearby: {
-        //gedung: [],
-        // nama: '',
-        // no_hp: '',
-        // jumlah_pesanan: '',
-        //total_harga: '',
-      },
-    };
-  },
-  mounted() {
-    axios
-      .get('https://officebooking-app-pn6n3.ondigitalocean.app/nearby')
-      .then((response) => {
-        this.nearby = response.data.data;
-        console.log(this.nearby);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  },
+	export default {
+		name: "profilAdmin",
+		components: {
+			Navbar,
+			Sidebar,
+			FooterComponent,
+			Breadcrumb,
+		},
+		data() {
+			return {
+				options: [{ value: null, text: "Pilih Gedung yang anda mau" }],
+				nearby: [],
+				loading: true,
+				items: [],
+				edit: false,
+				selected: null,
+			};
+		},
+		mounted() {
+			const data = JSON.parse(localStorage.getItem("EditPesanan"));
+			// console.log(data);
 
-  methods: {
-    async simpan() {
-      try {
-        const response = await axios.post('https://officebooking-app-pn6n3.ondigitalocean.app/nearby', this.nearby);
-        this.nearby = response.data;
-        this.nama = '';
-        this.no_hp = '';
-        this.jumlah_pesanan = '';
-        this.total_harga = '';
-      } catch (e) {
-        console.log(e);
-      }
-    },
-  },
-};
+			axios
+				.get("https://officebooking-app-pn6n3.ondigitalocean.app/admin/gedungs")
+				.then(response => {
+					let data = response.data.data;
+					data.forEach(item => {
+						this.options.push({
+							value: { id: item.id, name: item.name },
+							text: item.name,
+						});
+					});
+					// console.log(this.nearby);
+				})
+				.catch(error => {
+					console.log(error);
+				});
+			if (data) {
+				this.items = data;
+				this.edit = true;
+				// console.log(this.items, this.edit);
+				localStorage.removeItem("EditPesanan");
+				// alert("Data Masuk dan dihapus");
+			}
+			this.loading = false;
+		},
+
+		methods: {
+			async simpan() {
+				if (this.edit) {
+					let url =
+						"https://officebooking-app-pn6n3.ondigitalocean.app/admin/gedung" +
+						this.items.id;
+					await axios.put(url, this.items).then(res => {
+						console.log(res);
+						this.$router.push("/kelolaPesanan");
+					});
+				} else {
+					let url =
+						"https://officebooking-app-pn6n3.ondigitalocean.app/admin/gedung";
+					await axios.post(url, this.items).then(res => {
+						console.log(res);
+						this.$router.push("/kelolaPesanan");
+					});
+				}
+			},
+		},
+	};
 </script>
 
 <style scoped>
-p {
-  font-family: 'Poppins';
-  font-style: normal;
-  font-size: 19px;
-  font-weight: 600;
-  line-height: 33px;
-  display: flex;
-  color: #1cab59;
-}
-.card {
-  margin-right: 25px;
-  margin-left: 25px;
-  margin-bottom: 40px;
-  margin-top: 0px;
-}
-.Admin {
-  background-color: #e5e5e5 !important;
-  height: 100vh;
-}
-.card-body {
-  background-color: #fefefe;
-}
-.text {
-  text-align: left;
-  color: #1cab59 !important;
-}
-.text1 {
-  text-align: end;
-}
-.box {
-  height: 200px;
-  background: #fefefe !important;
-}
-svg {
-  width: 45px;
-  height: 45px;
-  margin: 0 auto;
-  display: block;
-}
-path {
-  fill: #f4f4f4 !important;
-}
-.card-text {
-  font-weight: 700;
-}
-button {
-  width: auto;
-  margin-right: 5px;
-}
-.btn-success {
-  background-color: white;
-  color: #1cab59 !important;
-  font-weight: bold;
-}
-.footer {
-  position: fixed !important;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-}
+	p {
+		font-family: "Poppins";
+		font-style: normal;
+		font-size: 19px;
+		font-weight: 600;
+		line-height: 33px;
+		display: flex;
+		color: #1cab59;
+	}
+	.card {
+		margin-right: 25px;
+		margin-left: 25px;
+		margin-bottom: 40px;
+		margin-top: 0px;
+	}
+	.Admin {
+		background-color: #e5e5e5 !important;
+		height: 100vh;
+	}
+	.card-body {
+		background-color: #fefefe;
+	}
+	.text {
+		text-align: left;
+		color: #1cab59 !important;
+	}
+	.text1 {
+		text-align: end;
+	}
+	.box {
+		height: 200px;
+		background: #fefefe !important;
+	}
+	svg {
+		width: 45px;
+		height: 45px;
+		margin: 0 auto;
+		display: block;
+	}
+	path {
+		fill: #f4f4f4 !important;
+	}
+	.card-text {
+		font-weight: 700;
+	}
+	button {
+		width: auto;
+		margin-right: 5px;
+	}
+	.btn-success {
+		background-color: white;
+		color: #1cab59 !important;
+		font-weight: bold;
+	}
+	.footer {
+		position: fixed !important;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+	}
 </style>
--- src/views/kelolaPesanan.vue
+++ src/views/kelolaPesanan.vue
@@ -1,127 +1,142 @@
+<!-- @format -->
+
 <template>
-  <div class="list">
-    <Navbar />
-    <Sidebar />
-    <br />
-    <br />
-    <br />
-    <Breadcrumb />
-    <b-card>
-      <p>Kelola Pesanan</p>
-      <b-table-simple striped hover>
-        <b-thead>
-          <b-tr>
-            <b-th>No</b-th>
-            <b-th>Kode Pemesanan</b-th>
-            <b-th>Nama Gedung</b-th>
-            <b-th>No Handphone</b-th>
-            <b-th>Jumlah Pesanan</b-th>
-            <b-th>Total Harga</b-th>
-            <b-th>Tanggal Masuk</b-th>
-            <b-th>Tanggal Keluar</b-th>
-            <b-th>Order Status</b-th>
-          </b-tr>
-        </b-thead>
-        <b-tbody>
-          <b-tr v-for="(booking, id) in bookings" :key="id">
-            <b-td>{{ booking.id }}</b-td>
-            <b-td>{{ booking.bookingcode }}</b-td>
-            <b-td>{{ booking.namagedung }}</b-td>
-            <b-td>{{ booking.nama }}</b-td>
-            <b-td>{{ booking.phone }}</b-td>
-            <b-td>{{ booking.jumlah_pesanan }}</b-td>
-            <b-td>{{ booking.total_harga }}</b-td>
-            <b-td>{{ booking.checkin }}</b-td>
-            <b-td>{{ booking.checkout }}</b-td>
-            <b-td>{{ booking.status }}</b-td>
-            <b-td>
-              <b-button variant="success" @click="okGedung(pesanan.id_gedung)">Terima</b-button>
-              <b-button v-b-modal.hapus variant="danger">Batalkan</b-button>
-              <b-modal id="hapus" centered busy>
-                <p class="my-4">Apakah Anda Yakin ?</p>
-                <b-button variant="danger" @click="cancelGedung(gedung.id)">Ya</b-button>
-                <b-button variant="success" @click="$bModal.hide('hapus')">Tidak</b-button>
-              </b-modal>
-            </b-td>
-          </b-tr>
-        </b-tbody>
-      </b-table-simple>
-    </b-card>
-    <liveChatBtn />
-    <FooterComponent />
-  </div>
+	<div class="list">
+		<Navbar />
+		<Sidebar />
+		<br />
+		<br />
+		<br />
+		<Breadcrumb />
+		<b-card v-if="loading">
+			<b-spinner variant="success" label="Spinning"></b-spinner>
+		</b-card>
+		<b-card v-else>
+			<p>Kelola Pesanan</p>
+			<b-table striped hover :items="items" :fields="fields">
+				<template #cell(Permintaan)="data">
+					<div v-if="data.item.Status === 'Belum Diterima'">
+						<b-button variant="success" @click="EditPesanan(data.item)"
+							>Edit</b-button
+						>
+						<b-button variant="info" @click="BCPesanan()">Kirim Bc</b-button>
+					</div>
+					<div v-else>Sudah Diterima</div>
+				</template>
+			</b-table>
+		</b-card>
+		<liveChatBtn />
+		<FooterComponent />
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Navbar from '@/components/navbarComponent.vue';
-import Sidebar from '@/components/sidebarComponent.vue';
-import Breadcrumb from '../components/breadcrumb.vue';
-import FooterComponent from '@/components/footerComponent.vue';
-import LiveChatBtn from '@/components/liveChatBtn.vue';
-import axios from 'axios';
+	// @ is an alias to /src
+	import Navbar from "@/components/navbarComponent.vue";
+	import Sidebar from "@/components/sidebarComponent.vue";
+	import Breadcrumb from "../components/breadcrumb.vue";
+	import FooterComponent from "@/components/footerComponent.vue";
+	import LiveChatBtn from "@/components/liveChatBtn.vue";
+	import axios from "axios";
 
-export default {
-  name: 'daftarPengguna',
-  components: {
-    Navbar,
-    Sidebar,
-    Breadcrumb,
-    FooterComponent,
-    LiveChatBtn,
-  },
-  data() {
-    return {
-      bookings: [],
-    };
-  },
-
-  mounted() {
-    axios
-      .get('https://officebooking-app-pn6n3.ondigitalocean.app/admin/bookings')
-      .then((response) => {
-        this.gedung = response.data.data;
-        console.log(this.gedung);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  },
-};
+	export default {
+		name: "daftarPengguna",
+		components: {
+			Navbar,
+			Sidebar,
+			Breadcrumb,
+			FooterComponent,
+			LiveChatBtn,
+		},
+		data() {
+			return {
+				fields: [
+					"ID_Pemesanan",
+					"Nama",
+					"NomorHp",
+					"ID_Email",
+					"Nama_Gedung",
+					"Jumlah_Pemesanan",
+					"Total_Harga",
+					"Tanggal_Masuk",
+					"Tanngal_Keluar",
+					"Permintaan",
+				],
+				items: [],
+				loading: true,
+			};
+		},
+		async mounted() {
+			await axios
+				.get(
+					"https://officebooking-app-pn6n3.ondigitalocean.app/admin/bookings"
+				)
+				.then(res => {
+					const data = res.data.data;
+					data.forEach(item => {
+						this.items.push({
+							ID: item.id,
+							ID_Pemesanan: item.bookingcode,
+							Nama: item.user.name || "Lupa Masukkan",
+							NomorHp: item.user.phone || "Lupa Masukkan",
+							Nama_Gedung: item.gedung.name || "Lupa Masukkan",
+							Jumlah_Pemesanan: item.gedung.jumlah || "Lupa Masukkan",
+							Total_Harga: item.gedung.harga || "Lupa Narok",
+							Tanggal_Masuk: item.checkin || "gk Mesan",
+							Tanngal_Keluar: item.checkout || "gk Mesan",
+							Status: item.status === "0" ? "Belum Diterima" : "Sudah Diterima",
+						});
+					});
+					this.loading = false;
+				})
+				.catch(e => {
+					console.log(e);
+					this.loading = false;
+				});
+		},
+		methods: {
+			EditPesanan(data) {
+				let simpan = JSON.stringify(data);
+				localStorage.setItem("EditPesanan", simpan);
+				this.$router.push("/addPesanan");
+				// console.log(data);
+			},
+		},
+	};
 </script>
 
 <style scoped>
-.list {
-  background-color: #e5e5e5;
-}
-.card {
-  margin-right: 25px;
-  margin-left: 25px;
-  margin-bottom: 25px;
-}
-p {
-  font-family: 'Poppins';
-  font-style: normal;
-  font-size: 19px;
-  font-weight: 600;
-  line-height: 33px;
-  display: flex;
-  color: #1cab59;
-}
-.btn-success {
-  background-color: #1cab59;
-  border-color: #1cab59;
-  width: 90px;
-  height: 31px;
-  margin: 10px !important;
-  padding: 0 !important;
-}
-.btn-danger {
-  background-color: #ff3538 !important;
-  border-color: #ff3538 !important;
-  width: 90px;
-  height: 31px;
-  margin: 10px !important;
-  padding: 0 !important;
-}
+	.list {
+		background-color: #e5e5e5;
+	}
+	.card {
+		margin-right: 25px;
+		margin-left: 25px;
+		margin-bottom: 25px;
+	}
+	p {
+		font-family: "Poppins";
+		font-style: normal;
+		font-size: 19px;
+		font-weight: 600;
+		line-height: 33px;
+		display: flex;
+		color: #1cab59;
+	}
+	.btn-success {
+		background-color: #1cab59;
+		border-color: #1cab59;
+		width: 90px;
+		height: 31px;
+		margin: 10px !important;
+		padding: 0 !important;
+	}
+	.btn-danger {
+		background-color: #ff3538 !important;
+		border-color: #ff3538 !important;
+		width: 90px;
+		height: 31px;
+		margin: 10px !important;
+		padding: 0 !important;
+	}
 </style>
